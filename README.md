eabc3dce27a49c873c63742fc18c1866edcf78c4
kentsin <kentsin@gmail.com>
2022-07-24 19:58:03 +0800

    password hash

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fastapi-tutorial
 Learning fastapi
+
+https://www.fastapitutorial.com/blogs/
+
+
+ - FastAPI
+ - uVicorn
+ - Jinja2
+ - PostGreSql
+ - sqlAlchemy
+ - sqlite
+ - Bcrypt
+ - pyDantic
--- backend/apis/general_pages/route_homepage.py
+++ backend/apis/general_pages/route_homepage.py
@@ -16,3 +16,7 @@ general_pages_router = APIRouter()
 @general_pages_router.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("general_pages/homepage.html", {"request":request})
+
+@general_pages_router.get("/about")
+async def home(request: Request):
+    return templates.TemplateResponse("general_pages/about.html", {"request":request})
