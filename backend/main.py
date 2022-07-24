2d578e29772c4cf616c5378bf0a3b516088abf92
kentsin <kentsin@gmail.com>
2022-07-24 11:41:23 +0800

    logo

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,13 @@ from apis.general_pages.route_homepage import general_pages_router
 def include_router(app):
     app.include_router(general_pages_router)
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"))
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
 
 app = start_application()
